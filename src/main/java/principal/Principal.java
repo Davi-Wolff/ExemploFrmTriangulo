1d0e570a89438b27e09534ea05518132a2a5eab6
Davi Wolff <>
2025-05-12 09:51:36 -0300

    Início do projeto.

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,14 @@
+package principal;
+/**
+ *
+ * @author Davi Wolff
+ */
+import visao.FrmTriangulo;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        FrmTriangulo janela = new FrmTriangulo();
+        janela.setVisible(true);
+    }
+}
